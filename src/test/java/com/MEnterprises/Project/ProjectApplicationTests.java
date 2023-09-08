8307852cdf07ecb7b38863874c95ccf44da908e6
ManojIN <25manoj05@gmail.com>
2023-09-08 22:04:23 +0530

    updated CI configs

--- src/test/java/com/MEnterprises/Project/ProjectApplicationTests.java
+++ src/test/java/com/MEnterprises/Project/ProjectApplicationTests.java
@@ -1,13 +1,30 @@
 package com.MEnterprises.Project;
 
+import com.example.helloworld.HelloWorldController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@SpringBootTest
+@SpringBootTest(classes = HelloWorldController.class)
+@AutoConfigureMockMvc
 class ProjectApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
 	@Test
-	void contextLoads() {
+	void TestEndpoint() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/hello").
+				contentType(MediaType.APPLICATION_JSON)).
+				andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
 	}
 
 }
